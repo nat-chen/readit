2d9d30db497c611dc45f127a0dba74db7cef44c7
Nat Chen <chen_natalie@outlook.com>
2021-09-05 10:12:08 +0800

    login, logout and auth middleware

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,20 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express from "express";
 import morgan from "morgan";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from './routes/auth';
 import trim from './middleware/trim';
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(trim);
+app.use(cookieParser());
 
 app.get("/", (_, res) => res.send("Hello world"));
 app.use('/api/auth', authRoutes);
