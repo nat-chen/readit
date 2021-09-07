d28e8007aaad487191c87fa4ccc6289bd1addd67
Nat Chen <chen_natalie@outlook.com>
2021-09-07 11:31:11 +0800

    register and login page

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 import authRoutes from './routes/auth';
 import postRoutes from './routes/posts';
@@ -19,6 +20,12 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(trim);
 app.use(cookieParser());
+app.use(cors({
+  credentials: true,
+  origin: process.env.ORIGIN,
+  optionsSuccessStatus: 200
+}));
+
 app.get("/", (_, res) => res.send("Hello world"));
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
