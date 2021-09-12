9a560afa15a8f02f158762cbff1cdf04cb7820e5
Nat Chen <chen_natalie@outlook.com>
2021-09-12 17:23:24 +0800

    sub page

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app'
 import Axios from 'axios';
 import { Fragment } from 'react';
 import { useRouter } from 'next/router';
+import { SWRConfig } from 'swr';
 
 import { AuthProvider } from '../context/auth';
 
@@ -13,16 +14,34 @@ import Navbar from '../components/Navbar'
 Axios.defaults.baseURL = 'http://localhost:5000/api'
 Axios.defaults.withCredentials = true;
 
+const fetcher = async (url: string) => {
+  try {
+    const res = await Axios.get(url)
+    return res.data
+  } catch (err) {
+    throw err.response.data
+  }
+}
+
 function App({ Component, pageProps }: AppProps) {
   const { pathname } = useRouter();
   const authRoutes = ['/register', '/login'];
   const authRoute = authRoutes.includes(pathname);
 
   return (
-    <AuthProvider>
-      {!authRoute && <Navbar />}
-      <Component {...pageProps} />
-    </AuthProvider>
+    <SWRConfig
+      value={{
+        fetcher,
+        dedupingInterval: 10000,
+      }}
+    >
+      <AuthProvider>
+        {!authRoute && <Navbar />}
+        <div className={authRoute ? '' : 'pt-12'}>
+          <Component {...pageProps} />
+        </div>
+      </AuthProvider>
+    </SWRConfig>
   );
 }
 
--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -1,46 +1,67 @@
-import { Router, Request, Response } from 'express';
-import { isEmpty } from 'class-validator';
-import { getRepository } from 'typeorm';
+import { Request, Response, Router } from "express";
+import { isEmpty } from "class-validator";
+import { getRepository } from "typeorm";
+import User from "../entities/User";
+import Sub from "../entities/Sub";
+import auth from "../middleware/auth";
+import user from "../middleware/user";
+import Post from "../entities/Post";
 
-import User from '../entities/User';
-import Sub from '../entities/Sub';
-import auth from '../middleware/auth';
-import user from '../middleware/user';
-
-const createSub = async(req: Request, res: Response) => {
+const createSub = async (req: Request, res: Response) => {
   const { name, title, description } = req.body;
   const user: User = res.locals.user;
   try {
     let errors: any = {};
-    if (isEmpty(name)) errors.name = 'Name must be empty';
-    if (isEmpty(title)) errors.title = 'Title must not be empty';
-
+    if (isEmpty(name)) errors.name = "Name must not be empty";
+    if (isEmpty(title)) errors.title = "Title must not be empty";
     const sub = await getRepository(Sub)
-      .createQueryBuilder('sub')
-      .where('lower(sub.name) = :name', { name: name.toLowerCase() })
+      .createQueryBuilder("sub")
+      .where("lower(sub.name) = :name", { name: name.toLowerCase() })
       .getOne();
-
-    if (sub) errors.name = 'Sub exists already';
-
+    if (sub) errors.name = "Sub exists already";
     if (Object.keys(errors).length > 0) {
       throw errors;
     }
   } catch (err) {
     return res.status(400).json(err);
   }
-
   try {
     const sub = new Sub({ name, description, title, user });
     await sub.save();
     return res.json(sub);
   } catch (err) {
     console.log(err);
-    return res.status(500).json({ error: 'Something went wrong' });
+    return res.status(500).json({ error: "Something went wrong" });
   }
-}
+};
+
+const getSub = async (req: Request, res: Response) => {
+  const name = req.params.name;
+
+  try {
+    const sub = await Sub.findOneOrFail({ name });
+    const posts = await Post.find({
+      where: { sub },
+      order: { createdAt: "DESC" },
+      relations: ["comments", "votes"],
+    });
+
+    sub.posts = posts;
+
+    if (res.locals.user) {
+      sub.posts.forEach((p) => p.setUserVote(res.locals.user));
+    }
+
+    return res.json(sub);
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ sub: "Sub not found" });
+  }
+};
 
 const router = Router();
 
-router.post('/', user, auth, createSub);
+router.post("/", user, auth, createSub);
+router.get("/:name", user, getSub);
 
 export default router;
